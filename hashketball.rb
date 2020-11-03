f5e83f3a1627b8b86448df66019a1fea85667e9f
Riley Iverson <rmiverson99@gmail.com>
2020-11-03 21:46:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,13 +6,61 @@ def game_hash
       :team_name => "",
       :colors => [""],
       :players => [
+        { 
+          :player_name =>,
+          :number =>,
+          :shoe =>,
+          :points =>,
+          :rebounds =>,
+          :assists =>,
+          :steals =>,
+          :blocks =>,
+          :slam_dunks
+        },
+        { 
+          :player_name =>,
+          :number =>,
+          :shoe =>,
+          :points =>,
+          :rebounds =>,
+          :assists =>,
+          :steals =>,
+          :blocks =>,
+          :slam_dunks
+        },
+        { 
+          :player_name =>,
+          :number =>,
+          :shoe =>,
+          :points =>,
+          :rebounds =>,
+          :assists =>,
+          :steals =>,
+          :blocks =>,
+          :slam_dunks
+        },
         {
           :player_name =>,
           :number =>,
           :shoe =>,
+          :points =>,
+          :rebounds =>,
+          :assists =>,
+          :steals =>,
+          :blocks =>,
+          :slam_dunks
         },
-        {}
-        
+        {
+          :player_name =>,
+          :number =>,
+          :shoe =>,
+          :points =>,
+          :rebounds =>,
+          :assists =>,
+          :steals =>,
+          :blocks =>,
+          :slam_dunks =>
+        }
       ]
     },
     :away => {
